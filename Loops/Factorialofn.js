571aa8e62d73b922e07d0c00bb0c3cf395fc390b
mishra-pankaj <immishrapankaj@gmail.com>
2025-10-07 22:18:37 +0530

    Loops

---
+++ Loops/Factorialofn.js
@@ -0,0 +1,9 @@
+function factofn(no){
+    let fact = 1
+    for(i=1;i<=no;i++){
+        fact= fact*i;
+    }
+    console.log(fact);
+
+}
+factofn(5)
---
+++ Loops/sumofn.js
@@ -0,0 +1,9 @@
+function sumofn(no){
+    let sum = 0;
+    for(i=1;i<=no;i++){
+        sum=sum+i;
+    }
+    console.log(sum);
+
+}
+sumofn(3)
